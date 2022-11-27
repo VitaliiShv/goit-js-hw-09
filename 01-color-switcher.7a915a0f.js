174b99d2016495840c96d852a3a09a64c29f8976
VitaliiShv <51060742+VitaliiShv@users.noreply.github.com>
2022-11-27 13:19:42 +0000

    Deploying to gh-pages from @ VitaliiShv/goit-js-hw-09@d4778545effa641bba124a25eb61d93797e8499b 🚀

---
+++ 01-color-switcher.7a915a0f.js
@@ -0,0 +1,2 @@
+let t=null;const e={body:document.querySelector("body"),startBtn:document.querySelector("[data-start]"),stopBtn:document.querySelector("[data-stop]")};function n(){e.body.style.backgroundColor=`#${Math.floor(16777215*Math.random()).toString(16)}`}e.startBtn.addEventListener("click",(function(){t=setInterval(n,1e3),e.startBtn.disabled=!0})),e.stopBtn.addEventListener("click",(function(){clearInterval(t),e.startBtn.disabled=!1}));
+//# sourceMappingURL=01-color-switcher.7a915a0f.js.map
