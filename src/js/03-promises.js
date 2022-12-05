50d23d305f8a02dfe816cf89a5f645adceb62cc6
Vitalii Shved <vitalyashved@gmail.com>
2022-12-05 08:05:21 +0200

    add promise generator

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,8 +1,56 @@
+const refs = {
+  delayField: document.querySelector('input[name=delay]'),
+  delayStepField: document.querySelector('input[name=step]'),
+  amountField: document.querySelector('input[name=amount]'),
+  submitBtn: document.querySelector('button[type=submit]'),
+  form: document.querySelector('.form'),
+};
+
+let delay;
+let delayStep;
+let amount;
+
+refs.form.addEventListener('focusout', getFormInputs);
+
+refs.form.addEventListener('submit', onFormSubmit);
+
 function createPromise(position, delay) {
-  const shouldResolve = Math.random() > 0.3;
-  if (shouldResolve) {
-    // Fulfill
-  } else {
-    // Reject
+  return new Promise((resolve, reject) => {
+    const shouldResolve = Math.random() > 0.3;
+
+    setTimeout(() => {
+      if (shouldResolve) {
+        resolve({ position, delay });
+      } else {
+        reject({ position, delay });
+      }
+    }, delay);
+  });
+}
+
+function onFormSubmit(event) {
+  event.preventDefault();
+
+  for (let i = 1; i <= amount; i += 1) {
+    createPromise(i, delay)
+      .then(({ position, delay }) =>
+        console.log(`✅ Fulfilled promise ${position} in ${delay}ms`)
+      )
+      .catch(({ position, delay }) =>
+        console.log(`❌ Rejected promise ${position} in ${delay}ms`)
+      );
+    delay += delayStep;
+  }
+}
+
+function getFormInputs(event) {
+  if (event.target === refs.delayField) {
+    delay = Number(event.target.value);
+  }
+  if (event.target === refs.delayStepField) {
+    delayStep = Number(event.target.value);
+  }
+  if (event.target === refs.amountField) {
+    amount = Number(event.target.value);
   }
 }
